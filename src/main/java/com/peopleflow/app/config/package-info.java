073a69d2ee25b89592de2db64c0709e95e669714
programmistjava <ol_su@protonmail.com>
2021-08-04 14:56:54 +0300

    Initial version of peopleFlow generated by JHipster-7.0.1

---
+++ src/main/java/com/peopleflow/app/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.peopleflow.app.config;
---
+++ src/main/java/com/peopleflow/app/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.peopleflow.app.web.rest;
